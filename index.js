1ca8e07ec2f17f17f39dcdcd9bc391be040546f0
ValeriaBodrova <110822668+ValeriaBodrova@users.noreply.github.com>
2023-05-31 11:17:43 +0200

    Created html

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ let name = prompt("What is your name?");
 if (name === "John") {
   alert("Hello, John! How are you?");
 }
+
+let number1 = prompt("Enter a number");
+let number2 = prompt("Enter second number");
+let operation = prompt("Select and enter the operation: "+" "-" "*" "/"");
+
+if (number1 > 0 && number2 > 0 && operation === "+") {
+    alert(`"Result:" ${number1} + ${number2}`);
+}
